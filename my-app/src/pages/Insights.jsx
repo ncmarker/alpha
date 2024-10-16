26d16026151057824807a7958d9781ebf641e123
ninamoothedath” <ninawm@gmail.com>
2024-10-15 17:36:33 -0700

    Manage Tasks & Insights

--- my-app/src/pages/Insights.jsx
+++ my-app/src/pages/Insights.jsx
@@ -1,9 +1,11 @@
+import { FullChartAnalysis } from '../components/Chart';
+
 export default function Insights() {
     return (
         <div className="flex flex-col w-full">
             <h1 className="mx-8 mt-4 text-4xl font-medium text-blue-600">Insights</h1>
             <div className="flex-grow flex flex-row flex-wrap p-4"> 
-                {/* add other components here */}
+            <FullChartAnalysis tasks={null}/>
             </div>
         </div>
     );
